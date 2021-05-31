1d366cfb408b5cd0b29159f491bd8f41764199d3
AleexWalker <aluurs4039@ieselcaminas.org>
2021-06-01 01:37:55 +0200

    Initial commit

---
+++ src/Aplicacion.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
+
+public class Aplicacion {
+    private JTextField txtDNI;
+    private JPanel panel1;
+    private JTextField textField2;
+    private JTextField txtNombre;
+    private JTextField txtCurso;
+    private JTextField txtNivel;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JComboBox cmbAsignaturas;
+    private JComboBox comboBox2;
+    private JButton btnEditar;
+    private JButton editarButton;
+    private JButton btnEliminar;
+    private JButton eliminarButton;
+    private JTextField quéQuieresBuscarTextField;
+    private JButton button5;
+    private JComboBox comboBox3;
+    private JLabel lblAsignaturas;
+    private JLabel lblNivel;
+    private JLabel lblCurso;
+    private JLabel lblNombre;
+    private JLabel lblDNI;
+
+
+}
