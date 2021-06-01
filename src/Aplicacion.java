c0cf5ca6d18c6387960d4cc71204522bc4e10bb0
AleexWalker <aluurs4039@ieselcaminas.org>
2021-06-01 02:03:37 +0200

    Swing

--- src/Aplicacion.java
+++ src/Aplicacion.java
@@ -3,21 +3,7 @@ import javax.swing.event.CaretEvent;
 import javax.swing.event.CaretListener;
 
 public class Aplicacion {
-    private JTextField txtDNI;
     private JPanel panel1;
-    private JTextField textField2;
-    private JTextField txtNombre;
-    private JTextField txtCurso;
-    private JTextField txtNivel;
-    private JTextField textField6;
-    private JTextField textField7;
-    private JTextField textField8;
-    private JComboBox cmbAsignaturas;
-    private JComboBox comboBox2;
-    private JButton btnEditar;
-    private JButton editarButton;
-    private JButton btnEliminar;
-    private JButton eliminarButton;
     private JTextField quéQuieresBuscarTextField;
     private JButton button5;
     private JComboBox comboBox3;
