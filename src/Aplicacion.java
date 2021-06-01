320ab36a65f72797d53673141323d3bddb188848
AleexWalker <aluurs4039@ieselcaminas.org>
2021-06-01 03:05:31 +0200

    Swing

--- src/Aplicacion.java
+++ src/Aplicacion.java
@@ -1,17 +1,32 @@
 import javax.swing.*;
 import javax.swing.event.CaretEvent;
 import javax.swing.event.CaretListener;
+import javax.swing.event.PopupMenuListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Aplicacion {
-    private JPanel panel1;
-    private JTextField quéQuieresBuscarTextField;
-    private JButton button5;
-    private JComboBox comboBox3;
-    private JLabel lblAsignaturas;
-    private JLabel lblNivel;
-    private JLabel lblCurso;
-    private JLabel lblNombre;
-    private JLabel lblDNI;
+    private JPanel pnlPrincipal;
+    private JTextField txtBuscar;
+    private JButton btnBuscar;
+    private JComboBox cmbSeleccion;
+    private JButton btnAñadir;
 
 
+    public Aplicacion() {
+        cmbSeleccion.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Aplicacion");
+        frame.setContentPane(new Aplicacion().pnlPrincipal);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
